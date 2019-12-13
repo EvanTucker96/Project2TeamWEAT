879cf7e54605c40fbcafbed460d41f38b2f62adc
tomrhollis <57675191+tomrhollis@users.noreply.github.com>
2019-12-13 16:43:19 -0700

    Basics of object oriented card deck
    needs lots more polish

---
+++ In Class/CardGame/CardGame/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CardGame
+{
+    public partial class frmCards : Form
+    {
+        Deck cardDeck = new Deck();
+        public frmCards()
+        {
+            InitializeComponent();
+            RefreshCards();
+        }
+
+
+        public void RefreshCards()
+        {
+            txtCardList.Text = cardDeck.ToString();
+        }
+
+        private void btnShuffle_Click(object sender, EventArgs e)
+        {
+            cardDeck.Shuffle();
+            RefreshCards();
+        }
+    }
+}
