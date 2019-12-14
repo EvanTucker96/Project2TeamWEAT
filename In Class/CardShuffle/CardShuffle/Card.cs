1b00f9ca905d966d6321ab64e36b1db8d7777d57
SinKane <wade.grimm@shaw.ca>
2019-12-13 18:10:09 -0700

    CardShuffle

---
+++ In Class/CardShuffle/CardShuffle/Card.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardShuffle
+{
+    public class Card
+    {
+        // Public data items as nothing changes
+        private enum Rank
+        { Ace = 1, Two, Three, Four, Five,
+            Six, Seven, Eight, Nine, Ten, Jack, Queen, King
+        }
+
+        private enum Suit { Club, Diamond, Heart, Spade }
+
+        // Properties
+        //public int FaceValue { get { return Rank; } }
+
+        //public static string GetSuit { get { return suit; } }
+
+        // Constructor
+
+
+        // Public operations
+        public override string ToString()
+        {
+           
+
+            return Rank + " of " + Suit;
+        } 
+
+    }
+}
