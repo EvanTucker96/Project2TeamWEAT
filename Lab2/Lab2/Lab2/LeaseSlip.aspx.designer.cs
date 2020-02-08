d68028de2720ee0256c6a70cabea3189043bd815
Thomas R. Hollis <654812@ACDM.DS.SAIT.CA>
2020-02-08 12:22:38 -0700

    Update LeaseSlip.aspx.designer.cs

--- Lab2/Lab2/Lab2/LeaseSlip.aspx.designer.cs
+++ Lab2/Lab2/Lab2/LeaseSlip.aspx.designer.cs
@@ -33,6 +33,42 @@ namespace Lab2
         protected global::System.Web.UI.WebControls.EntityDataSource EntityDataSource1;
 
         /// <summary>
+        /// TextBox2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+
+        /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+
+        /// <summary>
+        /// TextBox3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox3;
+
+        /// <summary>
+        /// TextBox4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox4;
+
+        /// <summary>
         /// lblDockID control.
         /// </summary>
         /// <remarks>
