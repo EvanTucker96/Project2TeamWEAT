2284aea634da0f7dbc783587a3ce4069771d282b
SinKane <wade.grimm@shaw.ca>
2020-02-09 20:04:00 -0700

    fixes

--- Lab2/Lab2/Lab2/LeaseSlip.aspx.cs
+++ Lab2/Lab2/Lab2/LeaseSlip.aspx.cs
@@ -11,41 +11,50 @@ namespace Lab2
     public partial class WebForm4 : System.Web.UI.Page
     {
         Customer cust; // object to hold info for the current customer
-        
+        string username;
 
         protected void Page_Load(object sender, EventArgs e)
         {
             MarinaEntities1 db = new MarinaEntities1(); // create DB object
-            if (!(bool)Session["Authenticated"]) // if they somehow got here without authentication, kick them back
+            try
+            {
+                if (!(bool)Session["Authenticated"]) // if they somehow got here without authentication, kick them back
+                {
+                    Response.Redirect("Registration.aspx");
+                }
+            }
+            catch (NullReferenceException)
             {
                 Response.Redirect("Registration.aspx");
             }
-
             // get the username from the session data
-            string username = (string)Session["Username"];
+            username = (string)Session["Username"];
 
+            GetLeases(username);
+
+        }
+
+        public void GetLeases(string username)
+        {
+            MarinaEntities1 db = new MarinaEntities1(); // create DB object
             // use the username to get the customer from the database (mostly for their ID)
             cust = db.Customers.Where(c => c.EMail == username).Single();
-            
+
             // get the slips that this customer has previously leased
             List<Slip> history = (from slip in db.Slips
-                                    join l in db.Leases on slip.ID equals l.SlipID
-                                    join c in db.Customers on l.CustomerID equals c.ID
-                                    where l.CustomerID == cust.ID
-                                    select slip).ToList();
+                                  join l in db.Leases on slip.ID equals l.SlipID
+                                  join c in db.Customers on l.CustomerID equals c.ID
+                                  where l.CustomerID == cust.ID
+                                  select slip).ToList();
 
             // put the list of this customer's leases in the DGV
             // this isn't working for some reason
             dgvLeases.DataSource = null;
             dgvLeases.DataSource = history;
-            
-            // bad, gross workaround to display the customer's lease history
-            foreach (Slip s in history)
-            {
-                lblTrash.Text = s.ID.ToString() + "</br>";
-            }
-
+            dgvLeases.DataBind();
         }
+
+
 
         protected void btnLease_Click(object sender, EventArgs e)
         {
@@ -76,6 +85,7 @@ namespace Lab2
                 // save to the database
                 db.Leases.Add(leased);
                 db.SaveChanges();
+                GetLeases(username);
             }
             
         }
