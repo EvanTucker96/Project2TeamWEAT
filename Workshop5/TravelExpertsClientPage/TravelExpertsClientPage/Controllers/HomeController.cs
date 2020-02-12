b785861c3dc184ccfd735cd38692e34171d6f018
SinKane <wade.grimm@shaw.ca>
2020-02-12 08:42:27 -0700

    more updates(again)

--- Workshop5/TravelExpertsClientPage/TravelExpertsClientPage/Controllers/HomeController.cs
+++ Workshop5/TravelExpertsClientPage/TravelExpertsClientPage/Controllers/HomeController.cs
@@ -30,43 +30,52 @@ namespace TravelExpertsClientPage.Controllers
 
         public ActionResult Register()
         {
-            ViewBag.Message = "Your Registration page.";
+            ViewBag.Message = "Customer registration page.";
 
             return View();
         }
 
+
+        /// <summary>
+        /// For Registering a new user
+        /// Author: WG
+        /// </summary>
+        /// <param name="cust">Passes in a Customer object from the form post</param>
+        /// <returns></returns>
         [HttpPost]
         public ActionResult Register(Customer cust)
         {
+            // setup a DataAccess object
             TravelExpertsEntities1 db = new TravelExpertsEntities1();
-            if (cust != null)
+            if (cust != null) // make sure the passed info exists
             {
-                List<Customer> customers = db.Customers.ToList();
-                int found = Convert.ToInt32((from c in customers
+                // Checks DB for existing record that matches First and Last names and Email address
+                int found = Convert.ToInt32((from c in db.Customers
                              where c.CustEmail == cust.CustEmail
                              && c.CustFirstName==cust.CustFirstName
                              && c.CustLastName==cust.CustLastName
                              select c.CustomerId).SingleOrDefault());
-                if (found == 0)
+                if (found == 0) // if no records found
                 {
+                    //Encrypt the password using BCrypt
                     cust.Password = cust.EncryptPassword(cust.Password);
+                    // set ComparePassword to encrypted password so validation passes
+                    // we've already verified they were the same before submit
                     cust.ComparePassword = cust.Password;
-                    db.Customers.Add(cust);
-                    db.SaveChanges();
-                    ViewBag.Status = "Registration Successful";
-                    return RedirectToAction("Index");
+                    db.Customers.Add(cust); // add the Customer record
+                    db.SaveChanges(); // comit the changes
+                    ViewBag.Status = "Registration Successful"; // set the Result status
+                    return RedirectToAction("Index"); // go back to 'Home'
                 }
                 else 
                 {
-                    ViewBag.Status = "User Exists";
-                    return RedirectToAction("Index");
+                    ViewBag.Status = "User Exists"; // set the Result status
+                    return RedirectToAction("Index"); // go back to 'Home'
                 }
-
-
             }
             else
             {
-                return View();
+                return View();// no data present, return to Regisrtation form
             }
         }
     }
