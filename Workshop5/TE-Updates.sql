0fa7057f43fe11b543e90ad934d68b73b73c197f
tomrhollis <57675191+tomrhollis@users.noreply.github.com>
2020-02-18 08:17:41 -0700

    Revert "Update TE-Updates.sql"
    This reverts commit 6220425288eba13bb4fc823aa3960570e9f56ff9.

--- Workshop5/TE-Updates.sql
+++ Workshop5/TE-Updates.sql
@@ -7,8 +7,6 @@ custbusphone nvarchar(20) null
 alter table Customers 
 add Password varchar(128) not null default '$2a$11$P7OHmF7BN95z56SkArdWI.SHEvsoJaeqzg1YFobDUP.saDAix6npi'
 
-/* Update existing customers Password column*/
-update Customers set password='$2a$11$P7OHmF7BN95z56SkArdWI.SHEvsoJaeqzg1YFobDUP.saDAix6npi'
 
 
 /* If Password column does exist in Agents, increase size from 50
